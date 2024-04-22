2829439ece02d579169cd6c164aa9eea2af0c71b
VaffanClash <52460508+VaffanClash@users.noreply.github.com>
2024-04-22 18:58:01 +0200

    cleaned forgetMessage.js

--- commands/general/forgetMessage.js
+++ commands/general/forgetMessage.js
@@ -1,40 +1,6 @@
 const fs = require('fs');
 const reactionMessages = require('../../reactionMessages.json');
 
-// module.exports = {
-//     name: 'forgetmessage',
-//     args: true,
-//     usage: '<CONFIG ID of the message you want to forget>',
-//     execute(message) {
-//         const msg = reactionMessages.messages;
-//         console.log('msg: ', msg);
-
-//         const args = message.content.split(/ +/);
-//         const messageToForget = Number(args[1]);
-
-//         msg.forEach(messaggio => {
-//             console.log(messaggio);
-//             if (messaggio[messageToForget] === undefined) {
-//                 console.log(messaggio[messageToForget]);
-//                 return;
-//             } else {
-//                 console.log(messaggio[messageToForget]);
-//                 // delete messaggio[messageToForget];
-//                 message.reply('message deleted')
-//             }
-//         })
-
-//         console.log(msg);
-//         const json = JSON.stringify(msg);
-        
-
-//         fs.writeFile("reactionMessages.json", json, function (err) {
-//             if (err) console.log("Error: " + err);
-//           });
-
-//     }
-// }
-
 module.exports = {
     name: 'forgetmessage',
     args: true,
